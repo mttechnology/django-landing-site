e15b55c862bd7148905c6b928cb308f72fe697b8
MEHRNAZ TOHIDI <123185346+mttechnology@users.noreply.github.com>
2025-11-16 10:23:42 +0330

    Add templates folder and render HTML pages in views
    Created a "templates" directory inside the app and added the required HTML files.
    Updated the views to render these templates using Django's render function.

--- website/views.py
+++ website/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'website/index.html')
 
 def about_view(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'website/about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1>Contact</h1>')
+    return render(request, 'website/contact.html')
