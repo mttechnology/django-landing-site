0594ea856463fcff396cf2a81ecd53228e7369bc
MEHRNAZ TOHIDI <123185346+mttechnology@users.noreply.github.com>
2025-11-25 17:01:10 +0330

    Add full template to project with static setup
    - Moved all HTML files, CSS, JS, and image assets into the project
    - Updated index.html with correct {% static %} tags
    - Verified that CSS and JS files load correctly

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home_view),
+    path('', index_view),
     path('about', about_view),
     path('contact', contact_view)
 ]
